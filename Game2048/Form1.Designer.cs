bbe79b1d7ccd7621e4e9d03d728a83fe95b0e047
Hariprasd Navilur N <hariprasadnavilurn@gmail.com>
2021-02-23 23:15:53 +0530

    loading from PC to github

---
+++ Game2048/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Game2048
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newGame = new System.Windows.Forms.Button();
+            this.b00 = new System.Windows.Forms.RichTextBox();
+            this.b01 = new System.Windows.Forms.RichTextBox();
+            this.b21 = new System.Windows.Forms.RichTextBox();
+            this.b22 = new System.Windows.Forms.RichTextBox();
+            this.b33 = new System.Windows.Forms.RichTextBox();
+            this.b23 = new System.Windows.Forms.RichTextBox();
+            this.b30 = new System.Windows.Forms.RichTextBox();
+            this.b20 = new System.Windows.Forms.RichTextBox();
+            this.b13 = new System.Windows.Forms.RichTextBox();
+            this.b12 = new System.Windows.Forms.RichTextBox();
+            this.b11 = new System.Windows.Forms.RichTextBox();
+            this.b10 = new System.Windows.Forms.RichTextBox();
+            this.b03 = new System.Windows.Forms.RichTextBox();
+            this.b02 = new System.Windows.Forms.RichTextBox();
+            this.b31 = new System.Windows.Forms.RichTextBox();
+            this.b32 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // newGame
+            // 
+            this.newGame.Location = new System.Drawing.Point(81, 13);
+            this.newGame.Name = "newGame";
+            this.newGame.Size = new System.Drawing.Size(75, 23);
+            this.newGame.TabIndex = 0;
+            this.newGame.Text = "New Game";
+            this.newGame.UseVisualStyleBackColor = true;
+            this.newGame.Click += new System.EventHandler(this.newGame_Click);
+            // 
+            // b00
+            // 
+            this.b00.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b00.Location = new System.Drawing.Point(81, 87);
+            this.b00.Name = "b00";
+            this.b00.Size = new System.Drawing.Size(100, 100);
+            this.b00.TabIndex = 1;
+            this.b00.Text = "";
+            // 
+            // b01
+            // 
+            this.b01.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b01.Location = new System.Drawing.Point(187, 87);
+            this.b01.Name = "b01";
+            this.b01.Size = new System.Drawing.Size(100, 100);
+            this.b01.TabIndex = 2;
+            this.b01.Text = "";
+            // 
+            // b21
+            // 
+            this.b21.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b21.Location = new System.Drawing.Point(187, 291);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(100, 100);
+            this.b21.TabIndex = 3;
+            this.b21.Text = "";
+            // 
+            // b22
+            // 
+            this.b22.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b22.Location = new System.Drawing.Point(293, 291);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(100, 100);
+            this.b22.TabIndex = 4;
+            this.b22.Text = "";
+            // 
+            // b33
+            // 
+            this.b33.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b33.Location = new System.Drawing.Point(399, 393);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(100, 100);
+            this.b33.TabIndex = 5;
+            this.b33.Text = "";
+            // 
+            // b23
+            // 
+            this.b23.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b23.Location = new System.Drawing.Point(399, 291);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(100, 100);
+            this.b23.TabIndex = 6;
+            this.b23.Text = "";
+            // 
+            // b30
+            // 
+            this.b30.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b30.Location = new System.Drawing.Point(81, 393);
+            this.b30.Name = "b30";
+            this.b30.Size = new System.Drawing.Size(100, 100);
+            this.b30.TabIndex = 7;
+            this.b30.Text = "";
+            // 
+            // b20
+            // 
+            this.b20.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b20.Location = new System.Drawing.Point(81, 291);
+            this.b20.Name = "b20";
+            this.b20.Size = new System.Drawing.Size(100, 100);
+            this.b20.TabIndex = 8;
+            this.b20.Text = "";
+            // 
+            // b13
+            // 
+            this.b13.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b13.Location = new System.Drawing.Point(399, 189);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(100, 100);
+            this.b13.TabIndex = 9;
+            this.b13.Text = "";
+            // 
+            // b12
+            // 
+            this.b12.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b12.Location = new System.Drawing.Point(293, 189);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(100, 100);
+            this.b12.TabIndex = 10;
+            this.b12.Text = "";
+            // 
+            // b11
+            // 
+            this.b11.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b11.Location = new System.Drawing.Point(187, 189);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(100, 100);
+            this.b11.TabIndex = 11;
+            this.b11.Text = "";
+            // 
+            // b10
+            // 
+            this.b10.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b10.Location = new System.Drawing.Point(81, 189);
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(100, 100);
+            this.b10.TabIndex = 12;
+            this.b10.Text = "";
+            // 
+            // b03
+            // 
+            this.b03.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b03.Location = new System.Drawing.Point(399, 87);
+            this.b03.Name = "b03";
+            this.b03.Size = new System.Drawing.Size(100, 100);
+            this.b03.TabIndex = 13;
+            this.b03.Text = "";
+            // 
+            // b02
+            // 
+            this.b02.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b02.Location = new System.Drawing.Point(293, 87);
+            this.b02.Name = "b02";
+            this.b02.Size = new System.Drawing.Size(100, 100);
+            this.b02.TabIndex = 14;
+            this.b02.Text = "";
+            // 
+            // b31
+            // 
+            this.b31.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b31.Location = new System.Drawing.Point(187, 393);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(100, 100);
+            this.b31.TabIndex = 15;
+            this.b31.Text = "";
+            // 
+            // b32
+            // 
+            this.b32.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b32.Location = new System.Drawing.Point(293, 393);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(100, 100);
+            this.b32.TabIndex = 16;
+            this.b32.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b02);
+            this.Controls.Add(this.b03);
+            this.Controls.Add(this.b10);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b20);
+            this.Controls.Add(this.b30);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b01);
+            this.Controls.Add(this.b00);
+            this.Controls.Add(this.newGame);
+            this.Name = "Form1";
+            this.Text = "2048";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newGame;
+        private System.Windows.Forms.RichTextBox b00;
+        private System.Windows.Forms.RichTextBox b01;
+        private System.Windows.Forms.RichTextBox b21;
+        private System.Windows.Forms.RichTextBox b22;
+        private System.Windows.Forms.RichTextBox b33;
+        private System.Windows.Forms.RichTextBox b23;
+        private System.Windows.Forms.RichTextBox b30;
+        private System.Windows.Forms.RichTextBox b20;
+        private System.Windows.Forms.RichTextBox b13;
+        private System.Windows.Forms.RichTextBox b12;
+        private System.Windows.Forms.RichTextBox b11;
+        private System.Windows.Forms.RichTextBox b10;
+        private System.Windows.Forms.RichTextBox b03;
+        private System.Windows.Forms.RichTextBox b02;
+        private System.Windows.Forms.RichTextBox b31;
+        private System.Windows.Forms.RichTextBox b32;
+    }
+}
+
---
+++ Game2048/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Game2048
+{
+    public partial class Form1 : Form
+    {
+        int[,] arr = new int[4, 4]; // By default zeroes
+        bool moved = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            addNumber();  // insert first number
+            addNumber();  // insert second number
+            display();
+            //gamePlay(); // start game
+        }
+        public int numberSelector()
+        {
+            Random rand = new Random();
+            if (rand.Next(0, 4) == 0)
+                return 4;
+            else 
+                return 2;
+        }
+        public int pickEmpty()
+        {
+            /* Return position of one empty cell */
+            int[] listOfEmpty = new int[16];
+            int k = 0;
+            Random rand = new Random();
+            // Find all empty cells
+            for (int r = 0; r < 4; r++)
+                for (int c = 0; c < 4; c++)
+                    if (arr[r, c] == 0)
+                        listOfEmpty[k++] = r * 4 + c;
+            // If no cell is empty, error
+            // Otherwise, return position of one of the empty cells
+            if (k == 0)
+                return -1;
+            else
+                return listOfEmpty[rand.Next(0,listOfEmpty.Length)];
+        }
+        void addNumber()
+        {
+            int pos = pickEmpty();
+            moved = false;
+            if(pos != -1)
+                arr[pos / 4, pos % 4] = numberSelector();
+        }
+        void gamePlay()
+        {
+            addNumber();
+            moved = false;
+            display();   
+        }
+        private void Form1_KeyPress(object sender, KeyEventArgs e)
+        {
+            int i, j, k;
+            if (e.KeyCode == Keys.Up)
+            {
+                for (j = 0; j < 4; j++) // column major
+                    for (i = 0; i < 3; i++)
+                    {
+                        if (arr[i, j] == arr[i + 1, j])
+                        {
+                            arr[i, j] *= 2;
+                            arr[i + 1, j] = 0;
+                            i++; // below cell is emptied in last statement. skip.
+                            moved = true;
+                        }
+                    }
+                for (j = 0; j < 4; j++)
+                    for (i = 0; i < 3; i++)
+                    {
+                        if (arr[i, j] == 0)
+                        {
+                            for (k = i + 1; k < 4 && arr[k, j] == 0; k++) ;
+                            if (k != 4)
+                            {
+                                arr[i, j] = arr[k, j];
+                                arr[k, j] = 0;
+                                moved = true;
+                            }
+                            else i = 3; // all cells below in the column are empty. so, skip to next column.
+                        }
+                    }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                for (j = 0; j < 4; j++)
+                    for (i = 3; i > 0; i--)
+                    {
+                        if (arr[i, j] == arr[i - 1, j])
+                        {
+                            arr[i, j] *= 2;
+                            arr[i - 1, j] = 0;
+                            i--; // above cell is emptied in last statement. skip.
+                            moved = true;
+                        }
+                    }
+                for (j = 0; j < 4; j++)
+                    for (i = 2; i >= 0; i--)
+                    {
+                        if (arr[i, j] == 0)
+                        {
+                            for (k = i - 1; k >= 0 && arr[k, j] == 0; k--) ;
+                            if (k != -1)
+                            {
+                                arr[i, j] = arr[k, j];
+                                arr[k, j] = 0;
+                                moved = true;
+                            }
+                            else i = -1; // all cells above in the column are empty. so, skip to next column.
+                        }
+                    }
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                for (i = 0; i < 4; i++)
+                    for (j = 0; j < 3; j++)
+                    {
+                        if (arr[i, j] == arr[i , j + 1])
+                        {
+                            arr[i, j] *= 2;
+                            arr[i , j + 1] = 0;
+                            j++; // right cell is emptied in last statement. skip.
+                            moved = true;
+                        }
+                    }
+                for (i = 0; i < 4; i++)
+                    for (j = 1; j < 4; j++)
+                    {
+                        if (arr[i, j] == 0)
+                        {
+                            for (k = j + 1; k < 4 && arr[i, k] == 0; k++) ;
+                            if (k != 4)
+                            {
+                                arr[i, j] = arr[i, k];
+                                arr[i, k] = 0;
+                                moved = true;
+                            }
+                            else j = 4; // all cells to right in the row are empty. so, skip to next row.
+                        }
+                    }
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                for (i = 0; i < 4; i++)
+                    for (j = 3; j > 0; j--)
+                    {
+                        if (arr[i, j] == arr[i, j - 1])
+                        {
+                            arr[i, j] *= 2;
+                            arr[i, j - 1] = 0;
+                            j--; // left cell is emptied in last statement. skip.
+                            moved = true;
+                        }
+                    }
+                for (i = 0; i < 4; i++)
+                    for (j = 3; j > 0; j--)
+                    {
+                        if (arr[i, j] == 0)
+                        {
+                            for (k = j - 1; k >= 0 && arr[i, k] == 0; k--) ;
+                            if (k != -1)
+                            {
+                                arr[i, j] = arr[i, k];
+                                arr[i, k] = 0;
+                                moved = true;
+                            }
+                            else j = 0; // all cells to right in the row are empty. so, skip to next row.
+                        }
+                    }
+            }
+            gamePlay();
+        }
+
+        private void newGame_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            for (i = 0; i < 4; i++)
+                for (j = 0; j < 4; j++)
+                    arr[i, j] = 0;
+            
+            addNumber();
+            addNumber();
+            moved = false;
+            //gamePlay();
+        }
+        private void display()
+        {
+            b00.Text = arr[0, 0].ToString();
+            b01.Text = arr[0, 1].ToString();
+            b02.Text = arr[0, 2].ToString();
+            b03.Text = arr[0, 3].ToString(); 
+            b10.Text = arr[1, 0].ToString();
+            b11.Text = arr[1, 1].ToString();
+            b12.Text = arr[1, 2].ToString();
+            b13.Text = arr[1, 3].ToString();
+            b20.Text = arr[2, 0].ToString();
+            b21.Text = arr[2, 1].ToString();
+            b22.Text = arr[2, 2].ToString();
+            b23.Text = arr[2, 3].ToString();
+            b30.Text = arr[3, 0].ToString();
+            b31.Text = arr[3, 1].ToString();
+            b32.Text = arr[3, 2].ToString();
+            b33.Text = arr[3, 3].ToString();
+        }
+    }
+}
